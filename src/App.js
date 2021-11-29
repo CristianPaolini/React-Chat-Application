f9fccdef962be1e060947facfe8e28860bbc358c
Cristian Paolini <cristianpaolini3@gmail.com>
2021-11-29 11:07:00 -0300

    Add ChatFeed Component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed.jsx'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         projectID="b8e59a98-3b68-4520-94e4-f3998846fcd1"
         userName="john"
         userSecret="qwerty"
+        renderChatFeed= { (chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage= { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
     </div>
   );
